8553fb256afb74726f775d74553d39c44aff312f
Endrit Kane <endritkane95@gmail.com>
2019-07-17 14:56:24 +0200

    fix pet names, breed

--- src/app.js
+++ src/app.js
@@ -13,9 +13,9 @@ const App = () => {
     {id: "something"}, // attr you want to give ex: id:"smth", id will be passed into div
     [
         React.createElement("h1", {}, "Adopt Me!"), //children
-        React.createElement(Pet, { name: "Gimi", animal: "Dog", breed: "hamshori"}),
-        React.createElement(Pet, { name: "Eli", animal: "Cat", breed: "legene"}),
-        React.createElement(Pet, { name: "Rosa", animal: "Bird", breed: "leshatore"})
+        React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese"}),
+        React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockateil"}),
+        React.createElement(Pet, { name: "Rosa", animal: "Cat", breed: "Persian"})
     ]);
 };
 
